be6a108bddf43b15365391c8e2911b1fa748d13a
Rafael Dias <rafadm2305@gmail.com>
2024-02-20 21:37:38 -0300

    ops

--- py/getCredentials.py
+++ py/getCredentials.py
@@ -6,26 +6,33 @@ def get_credentials():
     # global janela
     janela = tk.Tk()
     janela.title("Login")
+    grey = '#001A33'
+    lightGrey = '#DCDEE0'
+    darkBlue = '#393D5C'
+    # janela.wm_attributes('-toolwindow', 'True')
+    
     
     # Definindo o tamanho da janela
-    janela.geometry("300x110")  # Largura x Altura
-    # janela.configure(bg='black')
+    janela.geometry("220x110")  # Largura x Altura
+    janela.configure(bg=grey)
     window_width = int(janela.winfo_screenwidth())
     window_height = int(janela.winfo_screenheight())
+    # button_width = int(window_width*0.0125) #win10
+    button_width = int(window_width*0.007) #win11
     
     # Estilizando a janela
     style = ttk.Style()
     style.theme_use('clam')  # Escolha o tema que desejar
 
     # Criando os campos de entrada (entry)
-    label_login = tk.Label(janela, text="Login : ", padx=5)
+    label_login = tk.Label(janela, text="Login : ", padx=5, bg=grey, fg='white')
     label_login.grid(row=0, column=0, sticky=tk.E+tk.W, pady=5, padx=5)
-    entry_login = tk.Entry(janela)
+    entry_login = tk.Entry(janela,width=button_width)
     entry_login.grid(row=0, column=1)
 
-    label_senha = tk.Label(janela, text="Senha : ", padx=5)
+    label_senha = tk.Label(janela, text="Senha : ", padx=5, bg=grey, fg='white')
     label_senha.grid(row=1, column=0, sticky=tk.E+tk.W, pady=5, padx=5)
-    entry_senha = tk.Entry(janela, show="*")  # O parâmetro show="*" esconde os caracteres digitados
+    entry_senha = tk.Entry(janela, show="*",width=button_width)  # O parâmetro show="*" esconde os caracteres digitados
     entry_senha.grid(row=1, column=1)
 
     # Botão para submeter o login e senha
@@ -38,12 +45,9 @@ def get_credentials():
     # Ligando o evento de pressionar Enter ao botão de login
     def on_enter_pressed(event):
         on_login_click()
-    
-    # button_width = int(window_width*0.0125) #win10
-    button_width = int(window_width*0.007) #win11
-    button_height = int(20)
-    botao_login = tk.Button(janela, text="Login", padx=5, pady=5, command=on_login_click,width=button_width)
-    botao_login.grid(row=2, column=1)
+        
+    botao_login = tk.Button(janela, text="Login", command=on_login_click,width=button_width,bg=darkBlue,fg='white')
+    botao_login.grid(row=2, column=1, padx=5, pady=5)
     janela.bind('<Return>', on_enter_pressed)
 
     # Executando a interface
