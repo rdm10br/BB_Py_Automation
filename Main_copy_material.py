e6cccac2202bb6be1d668d439be1b22af796147a
Rafael Dias <rafadm2305@gmail.com>
2024-02-23 14:03:18 -0300

    att

--- Main_copy_material.py
+++ Main_copy_material.py
@@ -19,12 +19,19 @@ def run(playwright: Playwright) -> None:
     checkup_login.checkup_login(playwright)
     index = 0
     totalplan2 = getPlanilha.total_lines_plan2
+    context.new_page()
     
     for index in range(totalplan2) :
         index +=1
+        new_page = context.pages[1]
+        page = context.pages[0]
+        
+        page.close()
         
         copiaMaterial.copyMaterial(playwright,index)
         getPlanilha.writeOnExcel_Plan2(index,'OK')
+        
+        context.new_page()
         
     context.close()
     
