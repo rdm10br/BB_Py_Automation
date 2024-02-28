77f0b50185e309e13865083805ddd8a9b4d10ca9
Rafael Dias <rafadm2305@gmail.com>
2024-02-28 19:58:57 -0300

    k

--- Main_Ativp_teste.py
+++ Main_Ativp_teste.py
@@ -1,3 +1,12 @@
 curso = ["Engenharia Civil","Engenharia de Produção","Engenharia Elétrica","Engenharia Mecânica","Ciências Aeronáuticas"]
-cursos = len(curso)
-print(cursos)
+
+for i in range(len(curso)):
+    print(curso[i].lower())
+    print('============================')
+    print(curso[i].capitalize())
+    print('============================')
+    print(curso[i].upper())
+    print('============================')
+    print(curso[i].casefold())
+    print('============================')
+    i+=1
