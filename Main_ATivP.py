567d2ba02368760818425ce6fec753c17663783f
Rafael Dias <rafadm2305@gmail.com>
2024-02-28 18:01:11 -0300

    aeeeeeeeeeeew poha

--- Main_ATivP.py
+++ Main_ATivP.py
@@ -52,186 +52,186 @@ def run(playwright: Playwright) -> None:
             
             def inserirGruposAtividadesAV1(curso):
                 item = f'Envio AV1 - Atividade Prática de Extensão ({curso})'
-                new_page.get_by_role("link", name=item, exact=True).click()
+                searchURL = f'{classUrlUltra}?search={item}'
+                new_page.goto(searchURL)
                 new_page.get_by_role("button", name="Condições de liberação").click()
-                new_page.get_by_role("menuitem", name="Condições de liberação").click()
+                new_page.get_by_role("option", name="Condições de liberação").click()
+                new_page.get_by_label("Condições de liberação de").click()
                 new_page.get_by_label("Membros ou grupos específicos").check()
                 new_page.get_by_label("Membros ou grupos específicos").fill(curso)
                 new_page.get_by_role("button", name="Salvar").click()
                 new_page.wait_for_load_state('load')
-                new_page.goto(classUrlUltra)
-                new_page.wait_for_load_state('networkidle')
             
             def inserirGruposAtividadesAV2(curso):
-                item = f'Envio AV1 - Atividade Prática de Extensão ({curso})'
-                new_page.get_by_role("link", name=item, exact=True).click()
+                item = f'Envio AV2 - Atividade Prática de Extensão ({curso})'
+                searchURL = f'{classUrlUltra}?search={item}'
+                new_page.goto(searchURL)
                 new_page.get_by_role("button", name="Condições de liberação").click()
-                new_page.get_by_role("menuitem", name="Condições de liberação").click()
+                new_page.get_by_role("option", name="Condições de liberação").click()
+                new_page.get_by_label("Condições de liberação de").click()
                 new_page.get_by_label("Membros ou grupos específicos").check()
                 new_page.get_by_label("Membros ou grupos específicos").fill(curso)
                 new_page.get_by_role("button", name="Salvar").click()
                 new_page.wait_for_load_state('load')
-                new_page.goto(classUrlUltra)
-                new_page.wait_for_load_state('networkidle')
             
             if course_area == "['Educação I']" :
+                
                 gruposAtividades.inserirArquivoEducI(playwright ,id_interno)
+                print(course_area)
                 curso = ['Formação Pedagógica em Matemática para Graduados','Formação Pedagógica em Pedagogia para Graduados','Formação de Docente para a Educação Básica - Geografia','Formação de Docente para a Educação Básica - História','Formação de Docente para a Educação Básica - Letras','Formação de Docente para a Educação Básica - História','Segunda Licenciatura em Geografia','Segunda Licenciatura em História','Segunda Licenciatura em Letras - Espanhol','Segunda Licenciatura em Letras - Inglês','Segunda Licenciatura em Letras - Português','Segunda Licenciatura em Matemática','Segunda Licenciatura em Pedagogia']
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1               
                 
-            elif course_area == "['Educação II']":
+            if course_area == "['Educação II']":
+                
                 gruposAtividades.inserirArquivoEducII(playwright,id_interno)
+                print(course_area)
                 curso = ["Licenciatura em Educação Especial","Licenciatura em Educação Física","Geografia","História","Ciências Biológicas","Matemática","Letras - Espanhol","Letras - Inglês","Letras - Português"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Educação III']":
+            if course_area == "['Educação III']":
+                
                 gruposAtividades.inserirArquivoEducIII(playwright,id_interno)
+                print(course_area)
                 curso = ["Pedagogia"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Exatas']":
+            if course_area == "['Exatas']":
+                
                 gruposAtividades.inserirArquivoExat(playwright,id_interno)
+                print(course_area)
                 curso = ["Engenharia Civil","Engenharia de Produção","Engenharia Elétrica","Engenharia Mecânica","Ciências Aeronáuticas"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Negócios e Gestão I']":
+            if course_area == "['Negócios e Gestão I']":
+                
                 gruposAtividades.inserirArquivoNegI(playwright,id_interno)
+                print(course_area)
                 curso = ["Administração","Ciências Contábeis","Ciências Econômicas","Gestão Hospitalar"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Negócios e Gestão II']":
+            if course_area == "['Negócios e Gestão II']":
+                
                 gruposAtividades.inserirArquivoNegII(playwright,id_interno)
+                print(course_area)
                 curso = ["Gestão Financeira","Gestão Ambiental","Gastronomia","Gestão da Qualidade","Gestão Comercial com Complementação de Estudos em Gestão de E-Commerce","E-Commerce","Gestão de Recursos Humanos","Logística","Gestão de Trânsito","Gestão Comercial","Processos Gerenciais","Negócios Imobiliários"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Negócios e Gestão III']":
+            if course_area == "['Negócios e Gestão III']":
+                
                 gruposAtividades.inserirArquivoNegIII(playwright,id_interno)
+                print(course_area)
                 curso = ["Ciência Política","Gestão de Serviços Jurídicos e Notariais","Gestão Pública","Segurança Pública"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Saúde I']":
+            if course_area == "['Saúde I']":
+                
                 gruposAtividades.inserirArquivoSaudI(playwright,id_interno)
+                print(course_area)
                 curso = ["Farmácia","Enfermagem","Biomedicina"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Saúde II']":
+            if course_area == "['Saúde II']":
+                
                 gruposAtividades.inserirArquivoSaudII(playwright,id_interno)
+                print(course_area)
                 curso = ["Estética e Cosmética","Podologia"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Saúde III']":
+            if course_area == "['Saúde III']":
+                
                 gruposAtividades.inserirArquivoSaudIII(playwright,id_interno)
+                print(course_area)
                 curso = ["Bacharelado em Educação Física","Fisioterapia","Terapia Ocupacional","Nutrição"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Serviço Social e Teologia']":
+            if course_area == "['Serviço Social e Teologia']":
+                
                 gruposAtividades.inserirArquivoServ(playwright,id_interno)
+                print(course_area)
                 curso = ["Serviço Social","Teologia"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
-            elif course_area == "['Tecnologia da Informação']":
+            if course_area == "['Tecnologia da Informação']":
+                
                 gruposAtividades.inserirArquivoInfo(playwright,id_interno)
+                print(course_area)
                 curso = ["Analise e Desenvolvimento de Sistema","Ciência De Dados/Data Science","Coding","Computação Em Nuvem","Digital Security","Empreendedorismo Digital","Experiência do Usuário e Modelagem de Projetos Inovadores","Game Design","Tecnologia da Informação","Inteligência Artificial","Internet das Coisas","Service Design"]
                 
                 for i in range(len(curso)):
-                    gruposAtividades.openFolderAV1(playwright,id_interno)
                     inserirGruposAtividadesAV1(curso[i])
                     page.wait_for_load_state('load')
-                    gruposAtividades.openFolderAV2(playwright,id_interno)
                     inserirGruposAtividadesAV2(curso[i])
                     page.wait_for_load_state('load')
                     i+=1
                 
             else :
-                print(f'Grande Área da sala {id_externo} não identificada')
+                print(f'Grande Área da sala {id_externo} não identificada; {course_area}')
                 pass
             
             context.new_page()
---
+++ Main_Ativp_teste.py
@@ -0,0 +1,3 @@
+curso = ["Engenharia Civil","Engenharia de Produção","Engenharia Elétrica","Engenharia Mecânica","Ciências Aeronáuticas"]
+cursos = len(curso)
+print(cursos)
