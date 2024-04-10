a3bb2fabebca0a84939ae66fd651aed43e6ccd6a
Rafael Dias <rafadm2305@gmail.com>
2024-04-10 14:17:52 -0300

    att

--- Legado/teste/pysideqtcore.py
+++ Legado/teste/pysideqtcore.py
@@ -21,3 +21,12 @@ class MyWidget(QtWidgets.QWidget):
     @QtCore.Slot()
     def magic(self):
         self.text.setText(random.choice(self.hello))
+        
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
