5d17f34c03baf8ef2876eaed7866701ea928202b
Rafael Dias <rafadm2305@gmail.com>
2024-04-12 11:32:39 -0300

    att

--- Metodos/Master/AjusteSermelhor.py
+++ Metodos/Master/AjusteSermelhor.py
@@ -3,8 +3,9 @@ from playwright.async_api import Playwright, async_playwright, expect, Page
 
 async def ajusteSerMelhor(page: Page) -> None:
         
+    await page.wait_for_load_state('domcontentloaded')
     await page.press('body','End')
-    await page.press('body','End')
+    await page.wait_for_load_state('load')
     await page.get_by_label("Mais opções para SER Melhor (").click()
     await page.get_by_text("Editar", exact=True).click()
     await page.get_by_placeholder("Digite um URL").click(click_count=3)
