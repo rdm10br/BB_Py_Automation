660f1548a9963ebb392d70dd78f478a0ab436652
Rafael Dias <rafadm2305@gmail.com>
2024-04-12 11:36:44 -0300

    att

--- Metodos/Master/AjusteSermelhor.py
+++ Metodos/Master/AjusteSermelhor.py
@@ -5,6 +5,8 @@ async def ajusteSerMelhor(page: Page) -> None:
         
     await page.wait_for_load_state('domcontentloaded')
     await page.press('body','End')
+    await page.wait_for_load_state('domcontentloaded')
+    await page.press('body','End')
     await page.wait_for_load_state('load')
     await page.get_by_label("Mais opções para SER Melhor (").click()
     await page.get_by_text("Editar", exact=True).click()
