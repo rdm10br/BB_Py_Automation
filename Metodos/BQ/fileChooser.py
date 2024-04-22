e9bc1d5ed76addecf141e67457d91441418e6522
Rafael Dias <rafadm2305@gmail.com>
2024-04-22 17:06:02 -0300

    updates

--- Metodos/BQ/fileChooser.py
+++ Metodos/BQ/fileChooser.py
@@ -1,8 +1,10 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QFileDialog
 from PySide6.QtGui import QIcon
+# from PySide6.QtCore import pyqtSignal, SIGNAL
 import sys, os
 
 class MainWindow(QMainWindow):
+    # my_signal = pyqtSignal(str)
     def __init__(self):
         super().__init__()
 
@@ -24,7 +26,7 @@ class MainWindow(QMainWindow):
     def choose_file(self):
         todosArquivos = 'All Files (*)'
         arquivosTexto = 'Text File (*.docx *.doc)'
-        filename, _ = QFileDialog.getOpenFileName(
+        fileName, _ = QFileDialog.getOpenFileName(
             parent=self,
             caption="Escolha o arquivo",
             #apenas informando que a separação de cada opção de 
@@ -32,11 +34,13 @@ class MainWindow(QMainWindow):
             filter=f"{todosArquivos};;{arquivosTexto}",
             selectedFilter=arquivosTexto
             )
-        if filename:
-            print(f'\n O arquivo selecionado tem o caminho: {filename}\n')
+        if fileName:
+            print(f'\n O arquivo selecionado tem o caminho: {fileName}\n')
+            # self.emit(fileName)
             self.setDisabled(True)
-            # self.close()
+            self.close()
             # self.destroy()
+            return fileName
 
 
 def window_file():
@@ -44,7 +48,6 @@ def window_file():
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-    # app.exec()
 
 if __name__ == "__main__":
     window_file()
