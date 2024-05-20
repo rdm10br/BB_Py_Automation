0f5ca9629fcbffcdda7e3a34566d37849ecd2944
Rafael Dias <rafadm2305@gmail.com>
2024-05-20 12:35:02 -0300

    update classroom config new method

--- Test/Main_Classroom_Config.py
+++ Test/Main_Classroom_Config.py
@@ -12,39 +12,8 @@ async def run(page: Page, index) -> None:
     id_interno = await getFromAPI.API_Req(page=page, index=index)
 
     baseURL = 'https://sereduc.blackboard.com/'
-    classURL = f'{baseURL}ultra/courses/'
-    classUrlUltra = f'{classURL}{id_interno}/outline'
-
-    # API to review general content config from the classroom
-    APIContent = f'{baseURL}learn/api/public/v1/courses/{id_interno}/contents'
-
-    # API to review general config from gradebook
-    APIGradeCollum = f'{baseURL}learn/api/v1/courses/
-    {id_interno}/gradebook/columns'
-    
-    # API to review general config form gradebook filtered to Activity by name
-    APIGradeCollumActivity = f'{baseURL}learn/api/v1/courses/
-    {id_interno}/gradebook/columns?Name=Atividade%20de%20Autoaprendizagem'
-    
-    
-    # // visivel para o aluno
-    # "visible": true,
-
-    # // nota maxima no item
-    # "possible": 10,
-
-    # // quantidade de tentativas do item (0 = ilimitada)
-    # "multipleAttempts": 0,
-
-    # // visbilidade no boletim
-    # "visibleInBook": false,
-    
-    itemSearch = ''
-    # Needs exact name of the item to filter
-    filteredRequest = f'''
-    const data = JSON.parse(document.body.innerText).results
-    const filteredResults = data.filter(item => item.name === {itemSearch})[0]
-    console.log(filteredResults);'''
+    classURL = f'{baseURL}ultra/courses/{id_interno}'
+    classUrlUltra = f'{classURL}/outline'
     
     print(id_externo)
 
