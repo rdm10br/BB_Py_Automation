4bb7d4d3bc35543ac48cdf481c4d3b56ed88d626
Rafael Dias <rafadm2305@gmail.com>
2024-06-03 11:00:51 -0300

    update Main config doublecheck

---
+++ src/Main_config_doublecheck.py
@@ -0,0 +1,35 @@
+import asyncio
+from playwright.async_api import Page, expect
+
+
+from Metodos import getPlanilha, getFromAPI, getAPIContentConfig
+from Decorators.Main_StartUp import playwright_StartUp
+
+
+@playwright_StartUp
+async def run(page: Page, index) -> None:
+    
+        id_externo = getPlanilha.getCell(index=index)
+        id_interno = await getFromAPI.API_Req(page=page, index=index)
+        
+        baseURL = 'https://sereduc.blackboard.com/'
+        classURL = f'{baseURL}ultra/courses/'
+        classUrlUltra = f'{classURL}{id_interno}/outline'
+        
+        print(id_externo)
+        
+        # Masters DIG e TRAD
+        await getAPIContentConfig.doublecheck_config_main_Master(page=page, id_interno=id_interno)
+
+        # Mescla/Master DIG
+        # await getAPIContentConfig.doublecheck_config_main_DIG(page=page, id_interno=id_interno)
+
+        # Mescla/Master TRAD
+        # await getAPIContentConfig.doublecheck_config_main_TRAD(page=page, id_interno=id_interno)
+
+        
+async def main():
+    await run()
+
+
+asyncio.run(main())
