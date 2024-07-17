99d6d7640ecc123ca3b9de021f79a1f0aeb65384
Rafael Dias <rafadm2305@gmail.com>
2024-07-17 10:57:33 -0300

    UPDATE test legado and new UI

---
+++ UI/main/background.js
@@ -0,0 +1,52 @@
+import path from 'path'
+import { app, ipcMain } from 'electron'
+import serve from 'electron-serve'
+import { createWindow } from './helpers'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+if (isProd) {
+  serve({ directory: 'app' })
+} else {
+  app.setPath('userData', `${app.getPath('userData')} (development)`)
+}
+
+;(async () => {
+  // app.setActivationPolicy(policy='prohibited')
+  await app.whenReady()
+  
+
+  const mainWindow = createWindow('main', {
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+  
+  if (isProd) {
+    await mainWindow.loadURL('app://./home')
+  } else {
+    const port = process.argv[2]
+    await mainWindow.loadURL(`http://localhost:${port}/home`)
+    // await mainWindow.loadURL('https://sereduc.blackboard.com/ultra/admin')
+    // mainWindow.webContents.openDevTools()
+  }
+})()
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+ipcMain.on('message', async (event, arg) => {
+  event.reply('message', `${arg} World!`)
+})
+
+ipcMain.on('title', async (event, arg) => {
+  event.reply('title', `BBAutoPy - ${arg}`)
+  console.log(`BBAutoPy - ${arg}`)
+  // event.sender()
+  // event.returnValue()
+  // event.processId()
+  // event.frameId()
+})
