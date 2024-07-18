8f5a2f475bfc40ea650632933d555d9dad791943
Rafael Dias <rafadm2305@gmail.com>
2024-07-18 17:43:55 -0300

    update .....

--- UI/renderer/components/AppSideBar.js
+++ UI/renderer/components/AppSideBar.js
@@ -2,34 +2,35 @@ import React from "react";
 import Link from "next/link";
 // import { FaHome, FaInfoCircle, FaEnvelope } from 'react-icons/fa';
 import styles from "./AppSideBar.module.css";
-// import { Icon } from '@mui/material';
+
 
 
 const AppSideBar = () => {
   return (
     <div className={styles.sideMenu}>
       <ul>
-        <li className="title-head">
-          <img src="https://www.flaticon.com/free-icon/hamburger_2516745" />
+        <li>
+        {/* <li className="title-head"> */}
+          <img src="../styles/icon/home.png" />
           <p>
             APP ICON
           </p>
         </li>
         <li>
           <Link href="/home">
-            <img src="../style/icon/home.png" />
+            <img src="../styles/icon/home.png" />
             Home
           </Link>
         </li>
         <li>
           <Link href="/next">
-            <img src="../style/icon/home.png" />
+            <img src="../styles/icon/home.png" />
             Next
           </Link>
         </li>
         <li>
           <Link href="/run">
-            <img src="../style/icon/home.png" />
+            <img src="../styles/icon/home.png" />
             Run
           </Link>
         </li>
--- UI/renderer/pages/_app.jsx
+++ UI/renderer/pages/_app.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import AppSideBar from '../components/AppSideBar';
 import '../styles/global.css'
 
-// function MyApp({ Component, pageProps}) {
-//   return (<Component {...pageProps} />
-    
-//   )
-// }
-
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
