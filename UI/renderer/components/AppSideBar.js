fbfa6da4728671db827ecc72aa62c69166d34255
Rafael Dias <rafadm2305@gmail.com>
2024-07-19 16:00:17 -0300

    update .........

--- UI/renderer/components/AppSideBar.js
+++ UI/renderer/components/AppSideBar.js
@@ -1,31 +1,60 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./AppSideBar.module.css";
+import homeIcon from '../styles/icon/home.png';
 
 const AppSideBar = () => {
   return (
     <div className={styles.sideMenu}>
       <ul>
-        <li className="title-head">
-          <img src="../styles/icon/home.png"/>
+        <li className={styles.titleHead}>
+          <img src={homeIcon}/>
           <p>APP ICON</p>
+        </li>
+        <li className={styles.menu}>
+          <p>Menu</p>
         </li>
         <li>
           <Link href="/home" className={styles.link}>
-              <img src="../styles/icon/home.png"/>
+              <img src='assets/icon/home.png'/>
               Home
           </Link>
         </li>
         <li>
           <Link href="/next" className={styles.link}>
               <img src="../styles/icon/home.png"/>
-              Next
+              Double Check
           </Link>
         </li>
         <li>
           <Link href="/run" className={styles.link}>
               <img src="../styles/icon/home.png"/>
-              Run
+              Cópia
+          </Link>
+        </li>
+        <li>
+          <Link href="/home" className={styles.link}>
+              <img src="../styles/icon/home.png"/>
+              Data
+          </Link>
+        </li>
+        <li>
+          <Link href="/home" className={styles.link}>
+              <img src="../styles/icon/home.png"/>
+              X9
+          </Link>
+        </li>
+        <li>
+          <Link href="/home" className={styles.link}>
+              <img src="../styles/icon/home.png"/>
+              Teste
+          </Link>
+        </li>
+        <li className={styles.settings_span}></li>
+        <li>
+          <Link href="/home" className={styles.link}>
+              <img src="../styles/icon/home.png"/>
+              Configuração
           </Link>
         </li>
       </ul>
