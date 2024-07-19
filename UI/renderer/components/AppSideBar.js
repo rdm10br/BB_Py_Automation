6bbbc0572fd2ad5de89cc6fdb5868b0f47aab9f2
Rafael Dias <rafadm2305@gmail.com>
2024-07-19 10:00:52 -0300

    update link hole LI

--- UI/renderer/components/AppSideBar.js
+++ UI/renderer/components/AppSideBar.js
@@ -1,37 +1,31 @@
 import React from "react";
 import Link from "next/link";
-// import { FaHome, FaInfoCircle, FaEnvelope } from 'react-icons/fa';
 import styles from "./AppSideBar.module.css";
-
-
 
 const AppSideBar = () => {
   return (
     <div className={styles.sideMenu}>
       <ul>
-        <li>
-        {/* <li className="title-head"> */}
-          <img src="../styles/icon/home.png" />
-          <p>
-            APP ICON
-          </p>
+        <li className="title-head">
+          <img src="../styles/icon/home.png"/>
+          <p>APP ICON</p>
         </li>
         <li>
-          <Link href="/home">
-            <img src="../styles/icon/home.png" />
-            Home
+          <Link href="/home" className={styles.link}>
+              <img src="../styles/icon/home.png"/>
+              Home
           </Link>
         </li>
         <li>
-          <Link href="/next">
-            <img src="../styles/icon/home.png" />
-            Next
+          <Link href="/next" className={styles.link}>
+              <img src="../styles/icon/home.png"/>
+              Next
           </Link>
         </li>
         <li>
-          <Link href="/run">
-            <img src="../styles/icon/home.png" />
-            Run
+          <Link href="/run" className={styles.link}>
+              <img src="../styles/icon/home.png"/>
+              Run
           </Link>
         </li>
       </ul>
