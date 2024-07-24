f41083ecbf3e72fc3adac51e79e8dbd4ec1d2729
Rafael Dias <rafadm2305@gmail.com>
2024-07-24 13:28:25 -0300

    update organization

--- test/__init__.py
+++
@@ -1,5 +0,0 @@
-# from . import Main_doubleCheck_Mescla_DIG
-# from src import (Main_ajusteData, Main_BQ, Main_ATividades_Praticas,
-# Main_config_doublecheck, Main_copy_material, Main_copy_sala,
-# Main_doublecheck_Master, Main_Test, capture_console_output_async,
-# TimeStampedStream)
