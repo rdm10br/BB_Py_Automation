eac540ac7551a38dadb1d7118c757d43d0b417c3
Rafael Dias <rafadm2305@gmail.com>
2024-07-30 18:00:23 -0300

    update

---
+++ src/Metodos/Mescla/remove_ser.py
@@ -0,0 +1,20 @@
+from playwright.async_api import Page
+
+async def removeSer(page: Page, id_interno: str) -> None:
+    baseURL = 'https://sereduc.blackboard.com/'
+    item = 'Ser Melhor'
+    classurl = f'{baseURL}ultra/courses/outline?serach={item}'
+    await page.goto(url=classurl, wait_until='commit')
+    await page.wait_for_timeout(1000*4)
+    await page.wait_for_load_state('networkidle')
+    await page.wait_for_load_state('domcontentloaded')
+    await page.wait_for_load_state('load')
+    await page.wait_for_timeout(1000*6)
+    if await page.get_by_role("heading", name="Nenhum resultado encontrado").is_visible():
+        print(f'no {item} found')
+        pass
+    else:
+        print(f'{item} found')
+        await page.get_by_label('Mais opções para SER Melhor (').click()
+        await page.get_by_role('menuitem', name='Excluir').click()
+        await page.get_by_role('button', name='Excluir').click()
--- src/Metodos/__init__.py
+++ src/Metodos/__init__.py
@@ -2,5 +2,6 @@ from Metodos.API import checkupContent, getApiContent, getFromAPI, getPlanilha, 
 from Metodos.Copia import copiaMaterial, copiaSala
 from Metodos.Login import login, checkup_login, getCredentials
 from Metodos.Master import getData, ajusteData, AjusteSermelhor, AjusteSofiaV2, AjusteAvaliacaoV2
-from Metodos.Mescla import atribGrup, gruposAtividades, AjusteNotaZero, AjusteLinkEbook, openMescla
+from Metodos.Mescla import atribGrup, gruposAtividades, AjusteNotaZero, AjusteLinkEbook, openMescla, remove_ser
 from Metodos.BQ import getBQ, fileChooser, create_bq
+from Metodos.SURTOS import ajuste_AV1
