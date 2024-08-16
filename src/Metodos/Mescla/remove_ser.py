3465b8ec272538df63f3ec0d316d77361d392778
Rafael Dias <rafadm2305@gmail.com>
2024-08-16 11:39:59 -0300

    ✨ feat: now expect to show the deleted message

--- src/Metodos/Mescla/remove_ser.py
+++ src/Metodos/Mescla/remove_ser.py
@@ -1,4 +1,4 @@
-from playwright.async_api import Page
+from playwright.async_api import Page, expect
 
 async def removeSer(page: Page, id_interno: str) -> None:
     
@@ -22,4 +22,4 @@ async def removeSer(page: Page, id_interno: str) -> None:
         await page.get_by_role('button', name='Excluir').click()
         await page.wait_for_load_state('load')
         await page.wait_for_timeout(1000*3)
-        await expect(page.get_by_role("heading", name="Nenhum resultado encontrado").is_visible())
+        await expect(page.get_by_role("heading", name="Nenhum resultado encontrado")).to_be_visible()
