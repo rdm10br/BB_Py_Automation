aa9f2b9a61871ac0baf603fb2478a007049022e0
Rafael Dias <rafadm2305@gmail.com>
2024-08-27 15:58:58 -0300

    ⚡️ perf: copy new log and optmization

--- src/Main_copy_sala.py
+++ src/Main_copy_sala.py
@@ -1,4 +1,4 @@
-import asyncio, gc,  sys, os
+import asyncio, gc,  sys, os, time
 from playwright.async_api import Playwright, async_playwright, expect
 from dotenv import load_dotenv
 
@@ -12,9 +12,11 @@ async def run(playwright: Playwright) -> None:
     baseURL = os.getenv('BASE_URL')
     
     sys.stdout = TimeStampedStream(sys.stdout)
+    print('\nExecution Start')
     browser = await playwright.chromium.launch(headless=False, args=['--start-maximized'])
     context = await browser.new_context(base_url=baseURL, no_viewport=True)
     page = await context.new_page()
+    start_time0 = time.time()
 
     
     
@@ -22,6 +24,10 @@ async def run(playwright: Playwright) -> None:
     
     # Verificar se está logado e logar
     await checkup_login.checkup_login(page=page)
+    end_time0 = time.time()
+    execution_time = end_time0 - start_time0
+    executionTime0 = f'Execution time: {'{:.2f}'.format(execution_time)} seconds'
+    print(executionTime0)
     
     index = 0
     totalplan2 = getPlanilha.total_lines_plan2
@@ -31,9 +37,12 @@ async def run(playwright: Playwright) -> None:
     for index in range(totalplan2) :
         index +=1
         
+        print(f'Start loop {index}')
         cell_status = getPlanilha.getCell_plan2_status(index=index)
+        start_time = time.time()
         
         if cell_status != 'nan':
+            print(f'Index: {index} in plan is alredy writen')
             pass
         else :
             new_browser = await playwright.chromium.launch(headless=False, args=['--start-maximized'])
@@ -45,13 +54,22 @@ async def run(playwright: Playwright) -> None:
             await copiaSala.copySala(page=new_page, index=index)
             getPlanilha.writeOnExcel_Plan2(index=index, return_status='CRIADA')
             
+            
+            end_time = time.time()
+            execution_time = end_time - start_time
+            executionTime = f'Execution time: {'{:.2f}'.format(execution_time)} seconds'
+            print('{:5} | {}'.format(f'Run: {index}',executionTime))
             await new_context.close()
             await new_browser.close()
             
             gc.collect()
+            
+    print('Copy Finished')
         
     
 async def main():
     async with async_playwright() as playwright: # COLOCAR NAS OUTRAS
         await run(playwright)
+        
+        
 asyncio.run(main())
