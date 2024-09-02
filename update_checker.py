818ccf9646471e36b4d96ed4ba989cd84ebc8bd0
Rafael Dias <rafadm2305@gmail.com>
2024-09-02 17:55:19 -0300

    ✨ feat: auto updater

--- update_checker.py
+++ update_checker.py
@@ -1,17 +1,16 @@
-import requests, zipfile, os, json
+import requests, zipfile, os, json, tempfile, shutil
 
 
 def check_for_updates(current_version):
-    # response = requests.get('https://api.github.com/repos/rdm10br/BB_Py_Automation/releases/latest')
     response = requests.get('https://api.github.com/repos/rdm10br/BB_Py_Automation/releases/tags/pre-release')
     response.raise_for_status()
-    latest_version = response.json().get('tag_name').lstrip('v')
+    latest_version = response.json().get('name')
     return latest_version if latest_version > current_version else None
 
 
 def download_update(version):
-    # url = f'https://github.com/rdm10br/BB_Py_Automation/archive/refs/tags/v{version}.zip'
-    url = f'https://github.com/rdm10br/BB_Py_Automation/archive/refs/tags/{version}.zip'
+    # url = f'https://github.com/rdm10br/BB_Py_Automation/archive/refs/tags/{version}.zip'
+    url = 'https://github.com/rdm10br/BB_Py_Automation/archive/refs/tags/pre-release.zip'
     response = requests.get(url, stream=True)
     response.raise_for_status()
     with open('update.zip', 'wb') as file:
@@ -20,37 +19,68 @@ def download_update(version):
 
 
 def apply_update():
-    with zipfile.ZipFile('update.zip', 'r') as zip_ref:
-        zip_ref.extractall('update_directory')
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        with zipfile.ZipFile('update.zip', 'r') as zip_ref:
+            zip_ref.extractall(tmpdirname)
+        
+        # Determine if there is a single top-level directory in the extracted files
+        top_level_dirs = next(os.walk(tmpdirname))[1]
+        single_top_dir = top_level_dirs[0] if len(top_level_dirs) == 1 else None
+        
+        for root, dirs, files in os.walk(tmpdirname):
+            for filename in files:
+                src_file_path = os.path.join(root, filename)
+                
+                # Determine the relative path within the extracted structure
+                relative_path = os.path.relpath(src_file_path, tmpdirname)
+                
+                # # If there's a single top-level directory, adjust the relative path
+                # if single_top_dir:
+                relative_path = os.path.relpath(src_file_path, os.path.join(tmpdirname, single_top_dir))
+                
+                dest_file_path = os.path.join(os.getcwd(), relative_path)
+                
+                # Ensure the destination directory exists
+                os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
+                
+                # Move the file to the destination path
+                print(f'Updating {filename}...')
+                print(f'Updating to {dest_file_path}...')
+                shutil.move(src_file_path, dest_file_path)
     
-    for filename in os.listdir('update_directory'):
-        file_path = os.path.join('update_directory', filename)
-        if os.path.isfile(file_path):
-            os.replace(file_path, os.path.join('src', filename))  # Adjust path if needed
+    os.remove('update.zip')
 
 
 def restart_application():
-    # Restart the application or prompt user to restart manually
     print("Please restart the application to apply the update.")
 
 
 def main():
     CACHE_FILE = r'release.json'
     
-    with open(CACHE_FILE, 'r', encoding="utf-8") as f:
-        cache_data = json.load(f)
-        
-    CURRENT_VERSION = cache_data['CURRENT_VERSION']
+    try:
+        with open(CACHE_FILE, 'r', encoding="utf-8") as f:
+            cache_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("\nError reading the cache file.")
+        return
+    
+    CURRENT_VERSION = cache_data.get('CURRENT_VERSION')
+    if not CURRENT_VERSION:
+        print("\nCurrent version not found in cache.")
+        return
+    
     latest_version = check_for_updates(CURRENT_VERSION)
     
     if latest_version:
-        print(f'New version {latest_version} available. Updating...')
+        print(f'\nNew version {latest_version} available. Updating...')
         download_update(latest_version)
         apply_update()
-        restart_application()
+        # restart_application()
         print('Update complete.')
     else:
-        print('No updates available.')
+        print('\nNo updates available.')
+
 
 if __name__ == "__main__":
     main()
