181add1cd14338199fd07ba057ea0ea9008d667c
Rafael Dias <rafadm2305@gmail.com>
2024-09-24 15:23:20 -0300

    🚀 enhancement: Save credentials now deletes cookie cache on save

--- src/Main_Save_Login.py
+++ src/Main_Save_Login.py
@@ -9,6 +9,7 @@ async def run() -> None:
     username, password = getCredentials.get_credentials()
     print('credentials caugth...')
     cache_file = os.path.join(os.path.curdir, r'src\Metodos\Login\__pycache__\login.json')
+    cache_file_cookie = os.path.join(os.path.curdir, r'src\Metodos\Login\__pycache__\login_cache.json')
     cache_info = {
         "username": username,
         "password": password
@@ -18,6 +19,11 @@ async def run() -> None:
     with open(cache_file, 'w', encoding='UTF-8') as file:
         json.dump(cache_info, file, ensure_ascii=False, indent=4)
     print(f'login credentials cached at:\n{cache_file}')
+        
+    if os.path.exists(cache_file_cookie):
+        os.remove(cache_file_cookie)
+        print(f"Cookie File ({cache_file_cookie}) deleted successfully")
+        
 
 async def main():
     await run()
