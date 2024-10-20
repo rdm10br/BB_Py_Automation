862959d23e65729400f80f514c8e0bad63e11549
Rafael Dias <rafadm2305@gmail.com>
2024-10-20 08:58:15 -0300

    🚧 wip: Work in progress

--- setup.bat
+++ setup.bat
@@ -14,6 +14,22 @@ python -m spacy download pt_core_news_sm
 
 echo Setup complete. Activate the virtual environment with 'venv\Scripts\activate'.
 
+@REM echo Defining the .env variables
+@REM :: Define the variables
+@REM set BASE_URL=
+@REM set BQ_ID_REPOSITORY=
+@REM set GIT_REPO=rdm10br/BB_Py_Automation
+@REM set BRANCH=dev
+
+@REM echo Creating .env file
+@REM :: Create the .env file and write the variables to it
+@REM echo BASE_URL=%BASE_URL% >> .env
+@REM echo BQ_ID_REPOSITORY=%BQ_ID_REPOSITORY% >> .env
+@REM echo GIT_REPO=%GIT_REPO% >> .env
+@REM echo BRANCH=%BRANCH% >> .env
+
+@REM echo .env file created successfully!
+
 echo Setup schedule to updater.
 
 SET WorkingDir=%cd%
