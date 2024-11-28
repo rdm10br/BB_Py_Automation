1f9373915e6abe52cbd61d77f624304fcae83db7
jowke11 <bandeiradhiego@gmail.com>
2024-11-28 17:18:58 -0300

    ✨ Att fale com tutor

--- src/Metodos/Mescla/falecomtutor.py
+++ src/Metodos/Mescla/falecomtutor.py
@@ -11,6 +11,6 @@ async def falecomtutor(page: Page, id_interno: str) -> None:
     await page.locator("div").filter(has_text="Fale com o Tutor").first.click()
     await page.get_by_label("Mais opções", exact=True).click()
     await page.locator(f"#prompt-field{id_fct}_draw").click()
-    await page.get_by_text("Olá estudante!Esse canal é").press("ControlOrMeta+a")
+    await page.locator("#bb-editor-textbox").press("ControlOrMeta+a")
     await page.locator("#bb-editor-textbox").fill("Olá estudante!\nEsse canal é seu espaço pessoal para se comunicar reservadamente com o professor da disciplina. Fique a vontade para deixar aqui aquele recado que não quer tonar público!\nAté Breve!")
     await page.get_by_role("button", name="Salvar").click()
