7ef16c159f699b5b9e1b84ab88982f3f0fd6fab3
rdm10br <rafadm2305@gmail.com>
2025-01-07 12:28:55 -0300

    Att Ocultar

--- src/Metodos/Mescla/Ocultar.py
+++ src/Metodos/Mescla/Ocultar.py
@@ -45,16 +45,17 @@ async def ocultar_boletim(page: Page, id_interno: str) -> None:
         # await page.get_by_label("AV1").first.check()
         if is_ws != True:
             await page.get_by_role("row", name="AV1 Não está em um Período de avaliação Nota calculada").get_by_label("AV1").check(timeout=2*1000)
-            await page.wait_for_timeout(1000)
+            await page.wait_for_timeout(2*2000)
         await page.get_by_role("row", name="AV2 Não está em um Período de avaliação Nota calculada").get_by_label("AV2").check(timeout=2*1000)
-        await page.wait_for_timeout(1000)
+        await page.wait_for_timeout(2*2000)
         await page.get_by_role("row", name="AF Não está em um Período de avaliação Nota calculada").get_by_label("AF").check(timeout=2*1000)
-        await page.wait_for_timeout(1000)
+        await page.wait_for_timeout(2*2000)
         # await page.get_by_label("AV2").first.check()
         # await page.get_by_label("AF").first.check()
 
         await page.get_by_role("button", name="Mostrar/ocultar(Clique para").nth(1).hover()
         await page.get_by_role("menuitem", name="Ocultar colunas selecionadas", exact=True).click()
+        await page.wait_for_timeout(2*2000)
         await page.get_by_role("button", name="Enviar").click()
         await page.wait_for_load_state("networkidle")
         await page.wait_for_load_state('load')
@@ -64,23 +65,31 @@ async def ocultar_boletim(page: Page, id_interno: str) -> None:
         if is_ws != True:
             try:
                 await page.get_by_role("cell", name="AV1 (Oculto)").wait_for(state='visible', timeout=2000)
+                await page.wait_for_timeout(2*1000)
                 print('AV1 Oculto')
             except:
                 await page.get_by_role("row", name="AV1 Não está em um Período de avaliação Nota calculada").get_by_label("AV1").check(timeout=2*1000)
+                await page.wait_for_timeout(2*1000)
         try:
             await page.get_by_role("cell", name="AV2 (Oculto)").wait_for(state='visible', timeout=2000)
+            await page.wait_for_timeout(2*1000)
             print('AV2 Oculto')
         except:
             await page.get_by_role("row", name="AV2 Não está em um Período de avaliação Nota calculada").get_by_label("AV2").check(timeout=2*1000)
+            await page.wait_for_timeout(2*1000)
         try:
             await page.get_by_role("cell", name="AF (Oculto)").wait_for(state='visible', timeout=2000)
+            await page.wait_for_timeout(2*1000)
             print('AF Oculto')
         except:
             await page.get_by_role("row", name="AF Não está em um Período de avaliação Nota calculada").get_by_label("AF").check(timeout=2*1000)
+            await page.wait_for_timeout(2*1000)
         try:
             await page.get_by_role("button", name="Mostrar/ocultar(Clique para").nth(1).hover()
             await page.get_by_role("menuitem", name="Ocultar colunas selecionadas", exact=True).click()
+            await page.wait_for_timeout(2*2000)
             await page.get_by_role("button", name="Enviar").click()
+            await page.wait_for_load_state("networkidle")
             await page.wait_for_load_state('load')
             print('Sala Ajustada com sucesso')
         except:
