8ab01c286e0b6d89d5b367b3a32c5f0e660074f2
jowke11 <bandeiradhiego@gmail.com>
2025-01-07 09:01:38 -0300

    Att Ocultar

--- src/Metodos/Mescla/Ocultar.py
+++ src/Metodos/Mescla/Ocultar.py
@@ -4,11 +4,19 @@ from playwright.sync_api import Page
 async def ocultar_boletim(page: Page, id_interno: str) -> None:
     
     api = f'./learn/api/v1/courses/{id_interno}/gradebook/columns'
-    config = 'JSON.parse(document.body.innerText).results.find(item => item.columnName == "Avaliação Workshop")'
+    request = '''() => {
+    const data = JSON.parse(document.body.innerText).results;
+    if (data && data.length > 0 && data.find(item => item.columnName == "Avaliação Workshop")) {
+        return data.find(item => item.columnName == "Avaliação Workshop");
+    } else {
+        throw new Error('Item não encontrado');
+    }
+    }'''
+    # config = 'JSON.parse(document.body.innerText).results.find(item => item.columnName == "Avaliação Workshop")'
     
     await page.goto(api, wait_until='commit')
     try:
-        await page.evaluate(config)
+        await page.evaluate(request)
         is_ws = True
     except:
         is_ws = False
@@ -44,6 +52,7 @@ async def ocultar_boletim(page: Page, id_interno: str) -> None:
         await page.wait_for_timeout(1000)
         # await page.get_by_label("AV2").first.check()
         # await page.get_by_label("AF").first.check()
+
         await page.get_by_role("button", name="Mostrar/ocultar(Clique para").nth(1).hover()
         await page.get_by_role("menuitem", name="Ocultar colunas selecionadas", exact=True).click()
         await page.get_by_role("button", name="Enviar").click()
@@ -52,10 +61,26 @@ async def ocultar_boletim(page: Page, id_interno: str) -> None:
             
     except:
         if is_ws != True:
-            await page.get_by_role("cell", name="AV1 (Oculto)").wait_for(state='visible', timeout=2000)
-            print('AV1 Oculto')
-        await page.get_by_role("cell", name="AV2 (Oculto)").wait_for(state='visible', timeout=2000)
-        print('AV2 Oculto')
-        await page.get_by_role("cell", name="AF (Oculto)").wait_for(state='visible', timeout=2000)
-        print('AF Oculto')
-        
+            try:
+                await page.get_by_role("cell", name="AV1 (Oculto)").wait_for(state='visible', timeout=2000)
+                print('AV1 Oculto')
+            except:
+                await page.get_by_role("row", name="AV1 Não está em um Período de avaliação Nota calculada").get_by_label("AV1").check(timeout=2*1000)
+        try:
+            await page.get_by_role("cell", name="AV2 (Oculto)").wait_for(state='visible', timeout=2000)
+            print('AV2 Oculto')
+        except:
+            await page.get_by_role("row", name="AV2 Não está em um Período de avaliação Nota calculada").get_by_label("AV2").check(timeout=2*1000)
+        try:
+            await page.get_by_role("cell", name="AF (Oculto)").wait_for(state='visible', timeout=2000)
+            print('AF Oculto')
+        except:
+            await page.get_by_role("row", name="AF Não está em um Período de avaliação Nota calculada").get_by_label("AF").check(timeout=2*1000)
+        try:
+            await page.get_by_role("button", name="Mostrar/ocultar(Clique para").nth(1).hover()
+            await page.get_by_role("menuitem", name="Ocultar colunas selecionadas", exact=True).click()
+            await page.get_by_role("button", name="Enviar").click()
+            await page.wait_for_load_state('load')
+            print('Sala Ajustada com sucesso')
+        except:
+            print('Sala Ajustada com sucesso')
