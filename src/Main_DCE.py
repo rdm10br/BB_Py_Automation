f9feba019ede4e372945865ab669253e564c4d2d
rdm10br <rafadm2305@gmail.com>
2025-02-18 17:42:29 -0300

    ⏳ progress: Work in progress, not yet complete

--- src/Main_DCE.py
+++ src/Main_DCE.py
@@ -11,7 +11,7 @@ async def run(page: Page, index: int) -> None:
         id_interno = await getFromAPI.API_Req(page=page, index=index)
         
         await DCE_Mescla.adjust_name(page=page, id_interno=id_interno)
-        # await openMescla.open_Mescla(page=page, id_interno=id_interno)
+        await openMescla.close_Mescla(page=page, id_interno=id_interno)
         
         getPlanilha.writeOnExcel_Plan1(index=index, return_status='OK')
 
--- src/Metodos/Mescla/openMescla.py
+++ src/Metodos/Mescla/openMescla.py
@@ -17,3 +17,19 @@ async def open_Mescla(page: Page, id_interno: str) -> None:
     await page.get_by_role("button", name="Enviar").click()
     await page.wait_for_load_state('load')
     await page.wait_for_load_state('networkidle')
+    
+
+async def close_Mescla(page: Page, id_interno: str) -> None:
+    
+    url_edit = f'./webapps/blackboard/execute/editCourseManager?sourceType=COURSES&context=MODIFY&course_id={id_interno}'
+    await page.goto(url=url_edit, wait_until='commit')
+    
+    await page.locator('text=Para acessar a barra de ferramentas, pressione ALT+F10 (PC) ou ALT+FN+F10 (Mac).').wait_for(state='visible', timeout=1000*30)
+    await page.get_by_role("button", name="Enviar").wait_for(state='visible', timeout=1000*30)
+    await page.locator('#courseDesctext_tbl > tbody > tr > td > div').wait_for(state='visible', timeout=1000*30)
+    
+    await page.locator("#available_no").click()
+    await page.get_by_label("Usar definição de termo (").check()
+    await page.get_by_role("button", name="Enviar").click()
+    await page.wait_for_load_state('load')
+    await page.wait_for_load_state('networkidle')
