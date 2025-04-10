140317ea59563a18eaf80fcad22ff63077137398
carlosaugusto28 <carlos.augustoduarte12@hotmail.com>
2025-04-10 15:29:50 -0300

    Ajustes data fael
    morra black

---
+++ src/Metodos/Master/AjusteDataFael.py
@@ -0,0 +1,73 @@
+from playwright.async_api import Page
+
+
+async def ajusteData(page: Page) -> None:
+    """
+    Function that sets the ```dataShow``` and ```dataHide``` of an item
+
+    Args:
+        await page (Page): Page constructor form Playwright that
+        you want this Function to run
+        dataShow (str): Date to show the item in classroom
+        dataHide (str): Date to hide the item in classroom
+    """
+    # baseURL = "https://sereduc.blackboard.com/"
+    # rootURL = f'{baseURL}webapps/blackboard/execute/content/adaptiveReleaseRules?course_id={internalID}&content_id={contentID}'
+    
+    # await page.get_by_role("checkbox", name="Fale com o Tutor").check()
+    # await page.get_by_role("checkbox", name="Desafio Colaborativo").check()
+    
+    listaum =[
+        "Exercício de Fixação 01",
+        "Exercício de Fixação 02",
+        "Exercício de Fixação 03",
+        # "Exercício do Conhecimento",
+    ]# datas de entrega do dia 17
+    
+    
+    listadois =[
+        "Avaliação Workshop",
+        "Avaliação Discursiva",
+    ]# datas de entrega do dia 10
+    
+    
+    listatres =[
+        "Fale com o tutor"
+    ]# condição de liberação dia 17
+    
+    
+    listaquatro =[
+        "Converse com a sua Turma"
+    ]# condição de liberação dia 10
+    
+    
+    listacinco =[
+        "Avaliação Workshop"
+    ]# condição de liberação dia 20
+    
+    
+    await page.get_by_role("row", name="​📝​ Avaliações e Exercícios").get_by_label("row.openFolder").click()
+    await page.get_by_text("Exercícios de Fixação").click()
+    
+    
+    
+    for item in listaum:
+        await page.get_by_role("checkbox", name=item).check()
+        await page.wait_for_timeout(2500)
+        ...
+        
+        
+        
+    
+    await page.get_by_role("button", name="Editar datas", exact=True).click()
+    await page.get_by_label("Tipo de edição").click()
+    await page.get_by_role("option", name="Alterar para data e/ou hora").click()
+    await page.get_by_role("checkbox", name="Data de entrega").check()
+    await page.get_by_role("textbox", name="Data de entrega").fill("17/05/25")
+    await page.get_by_role("checkbox", name="Hora de entrega").check()
+    await page.get_by_role("textbox", name="Hora de entrega").fill("23:59")
+    # await page.get_by_role("button", name="Editar datas").click()
+    
+
+    # page.get_by_text("Avaliação Exercício do").click()
+    # page.get_by_role("checkbox", name="Exercício do Conhecimento", exact=True).check()
