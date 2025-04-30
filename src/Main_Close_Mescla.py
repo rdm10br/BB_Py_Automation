1d69d7047eb9459e9f20c693371b114161a5d22c
rdm10br <rafadm2305@gmail.com>
2025-04-30 17:29:24 -0300

    ✨ feature-add: Introducing a new feature close mescla

---
+++ src/Main_Close_Mescla.py
@@ -0,0 +1,22 @@
+import asyncio
+from playwright.async_api import Page
+
+from Metodos import getFromAPI, openMescla, getPlanilha
+from Decorators.Main_StartUp import playwright_StartUp_nosub
+
+
+@playwright_StartUp_nosub()
+async def run(page: Page, index: int) -> None:
+    
+        id_interno = await getFromAPI.API_Req(page=page, index=index)
+        
+        await openMescla.close_Mescla(page=page, id_interno=id_interno)
+        
+        getPlanilha.writeOnExcel_Plan1(index=index, return_status='OK')
+
+
+async def main():
+    await run()
+
+
+asyncio.run(main())
